bfd203e77bc56bbd6fd3dd7ef57fe433b674b605
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-12 22:30:26 -0600

    SamsungQualcommRil: bring back operatorCheck. (1/2)
    * use more memory effience algorithim.
    * Add Alot of comments
    * Add xml to hashmap code.
    * remove obsolete code.
    
    Change-Id: Ia567d376962d95fced93634ace52452cdaeafd8b

--- src/java/com/android/internal/telephony/SamsungQualcommRIL.java
+++ src/java/com/android/internal/telephony/SamsungQualcommRIL.java
@@ -559,15 +559,9 @@ public class SamsungQualcommRIL extends RIL implements CommandsInterface {
     private Object
     operatorCheck(Parcel p) {
         String response[] = (String[])responseStrings(p);
-        for(int i=0; i<3; i++){
+        for(int i=0; i<2; i++){
             if (response[i]!= null){
-                if (i<2){
-                    if (response[i].equals("       Empty") || (response[i].equals("") && !isGSM)) {
-                        response[i]=operator;
-                    }
-                } else if (response[i].equals("31000")|| response[i].equals("11111") || response[i].equals("123456") || response[i].equals("31099") || ((response[i].length()<5  || response[i].length()>6) && !isGSM)){
-                        response[i]=homeOperator;
-                }
+                response[i] = Operators.operatorReplace(response[i]);
             }
         }
         return response;
